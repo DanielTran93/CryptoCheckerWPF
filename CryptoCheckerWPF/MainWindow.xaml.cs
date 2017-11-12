8a7f2963eaad1d6e3d0c9dbeae0d64d3da29a68a
Dan Tran <daniel_t4221@hotmail.com>
2017-11-12 15:41:51 +1300

    Creation of WPF app

---
+++ CryptoCheckerWPF/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CoinMarketCapWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        async void getPrice_Click(object sender, RoutedEventArgs e)
+        {
+            string crypto = cryptoName.Text;
+            using (HttpClient client = new HttpClient())
+            {
+                using (HttpResponseMessage response = await client.GetAsync("https://api.coinmarketcap.com/v1/ticker/" + crypto))
+                {
+                    using (HttpContent content = response.Content)
+                    {
+                        string myContent = await content.ReadAsStringAsync();
+                        cryptoOutput.Text = myContent;
+                    }
+
+                }
+            }
+
+        }
+
+    }
+}
