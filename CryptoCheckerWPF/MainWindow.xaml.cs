e42a6e4348630e5b07ac995f2adeeec63a6db3f7
Daniel Tran <daniel_t4221@hotmail.com>
2017-12-29 14:20:01 +1300

    Only obtain the id,price, market cap and price changes. instead of 1 ugly list of everything from json api.

--- CryptoCheckerWPF/MainWindow.xaml.cs
+++ CryptoCheckerWPF/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
@@ -19,6 +21,8 @@ namespace CoinMarketCapWPF
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    /// 
+
     public partial class MainWindow : Window
     {
         public MainWindow()
@@ -26,6 +30,26 @@ namespace CoinMarketCapWPF
             InitializeComponent();
         }
 
+        public class CoinValues
+        {
+
+            public string id { get; set; }
+            public string name { get; set; }
+            public string symbol { get; set; }
+            public string rank { get; set; }
+            public string price_usd { get; set; }
+            public string price_btc { get; set; }
+            public string __invalid_name__24h_volume_usd { get; set; }
+            public string market_cap_usd { get; set; }
+            public string available_supply { get; set; }
+            public string total_supply { get; set; }
+            public string max_supply { get; set; }
+            public string percent_change_1h { get; set; }
+            public string percent_change_24h { get; set; }
+            public string percent_change_7d { get; set; }
+            public string last_updated { get; set; }
+
+        }
 
         async void getPrice_Click(object sender, RoutedEventArgs e)
         {
@@ -37,7 +61,16 @@ namespace CoinMarketCapWPF
                     using (HttpContent content = response.Content)
                     {
                         string myContent = await content.ReadAsStringAsync();
-                        cryptoOutput.Text = myContent;
+                        List<CoinValues> json = JsonConvert.DeserializeObject<List<CoinValues>>(myContent);
+                        cryptoID.Text = json[0].id;
+                        cryptoPrice.Text = "$"+json[0].price_usd;
+                        cryptoMarketCap.Text = "$"+json[0].market_cap_usd;
+                        crypto1HrChange.Text = json[0].percent_change_1h + "%";
+                        crypto24HrChange.Text = json[0].percent_change_24h + "%";
+                        crypto7DayChange.Text = json[0].percent_change_7d + "%";
+
+                        //need to catch exception when coin is not valid.
+
                     }
 
                 }
